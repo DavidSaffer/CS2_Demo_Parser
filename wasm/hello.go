152c8595ec6c7aaa86dff24a267bb6996fe7d5d7
David <davidsaffer@vt.edu>
2024-07-24 01:39:13 -0400

    try again

--- wasm/hello.go
+++ wasm/hello.go
@@ -135,8 +135,8 @@ func AnalyzeDemo(data []byte, attackerThreshold, victimThreshold int) {
 			playerStats[killerName].EcoKillRounds = append(playerStats[killerName].EcoKillRounds, roundNum) // Record the round number
 			updateMessage := fmt.Sprintf("Eco Kill - Round: %d Killer: %s Value :$ %d, Victim Value: $ %d", roundNum, killerName, killerValue, victimValue)
 			js.Global().Call("postMessage", updateMessage)
-			updateMessage = fmt.Sprintf("Eco Kill -Round: %d Killer HMTP: %t Victim HMTP: %t", roundNum, killerHasMoreThanPistol, victimHasMoreThanPistol)
-			js.Global().Call("postMessage", updateMessage)
+			updateMessage2 := fmt.Sprintf("Eco Kill -Round: %d Killer HMTP: %t Victim HMTP: %t", roundNum, killerHasMoreThanPistol, victimHasMoreThanPistol)
+			js.Global().Call("postMessage", updateMessage2)
 		}
 	})
 
